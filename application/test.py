de4ceecb0ca97f869dc8b8fbf67a27a8284223fc
dbonill <danbonilla002@gmail.com>
2022-03-13 14:26:04 -0500

    more test coverage

--- application/test.py
+++ application/test.py
@@ -14,6 +14,38 @@ class FlaskTest(unittest.TestCase):
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
 
+    def test_index3(self): #signup page
+        tester = app.test_client(self)
+        response = tester.post('/signup', data=dict(email='data1', name='data2name', password='data2pass'))
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 302) #redirect
+
+    def test_index4(self): #calculator page
+        tester = app.test_client(self)
+        response = tester.post('/quoteCalc', data=dict(zipcode='77204', gallons='420'))
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_index5(self): #submit to form
+        tester = app.test_client(self)
+        response = tester.post('/submitFormToDB', data=dict(dest='swag', zipcode='77204', gallons='1337'))
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_index6(self): #new customer submission
+        tester = app.test_client(self)
+        response = tester.post('/NewCustomerForm', data=dict(f_name = 'jim',
+                                                             l_name = 'tim',
+                                                             email = 'swag@swaggerson.com',
+                                                             newpasswd = 'ayoayoaoyaoy',
+                                                             phoneNum = '1234567891',
+                                                             Address = 'swagstreet',
+                                                             City = 'swagcity',
+                                                             state = 'TX',
+                                                             zipcode = '12345'))
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
 
     # #FAILED DUE TO OVERLOADING ROUTES (ONE HAS POST)
     # def test_index3(self):  # load signup page
